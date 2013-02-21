1e836dee38b95404f07648f42e35616f7da04cde
Marcelo Munhoz Pélos <mpelos@gmail.com>
2013-02-21 14:29:01 -0300

    Inserts brazilian portuguese locale

---
+++ locale/jquery.datenizer.pt-br.js
@@ -0,0 +1,14 @@
+$.datenizer.defaults.format = "%d/%m/%Y"
+
+$.datenizer.locale['pt-BR'] = {
+    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+
+    abbrMonthNames: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul',
+        'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta',
+        'Sábado'],
+
+    abbrDayNames: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+}
